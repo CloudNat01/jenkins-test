cde7fc2eaa9bcd425013ec0a6816aeebb8c42a81
cloudnat <92193921+CloudNat01@users.noreply.github.com>
2022-09-08 10:20:33 -0400

    Update bucket.py

--- bucket.py
+++ bucket.py
@@ -1,6 +1,6 @@
 import boto3
-from boto3 import Session
-session = boto3.Session(profile_name="admin", region_name="us-east-1")
+#from boto3 import Session
+session = boto3.Session(profile_name="admin")
 
 s3 = boto3.client("s3")
 s3.create_bucket(Bucket="loveth-oballe-buck-xl")
