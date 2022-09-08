279743654120689a41ee9ebe0fb72ded8a17cb92
cloudnat <92193921+CloudNat01@users.noreply.github.com>
2022-09-08 12:47:53 -0400

    Update bucket.py

--- bucket.py
+++ bucket.py
@@ -1,7 +1,16 @@
-import boto3
-#from boto3 import Session
-session = boto3.Session(profile_name="default")
+# import boto3
+# #from boto3 import Session
+# session = boto3.Session(profile_name="default")
 
-s3 = boto3.client("s3")
-s3.create_bucket(Bucket="loveth-oballe-buck-xl")
+# s3 = boto3.client("s3")
+# s3.create_bucket(Bucket="loveth-oballe-buck-xl")
                                            
+import boto3
+
+sess= boto3.Session(region_name='us-east-2')
+s3client = sess.client('s3')
+bucket_name='prudential-s3'
+s3_location={
+    'LocationConstraint': 'us-east-2'
+}
+s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
